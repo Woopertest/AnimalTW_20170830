ef39e6d4fc0349d2981f8078ef324b60a93fc130
Woopertest <toprockhui@gmail.com>
2017-10-06 10:37:07 +0800

    add_MenuSpan

--- js/base.js
+++ js/base.js
@@ -1,4 +1,6 @@
 function menu_btn(){
 	var menu_btn = document.getElementById("gnav");
+	var cancle_menu = document.getElementById("gnav_span");
 	menu_btn.classList.toggle("hide");
+	cancle_menu.classList.toggle("hide");
 }
